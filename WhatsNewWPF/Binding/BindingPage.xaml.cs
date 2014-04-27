10cb6e897eb9dbe101d2bdceff871a2b9a380640
Ben Dewey <bdewey01@hotmail.com>
2014-04-26 23:49:08 -0400

    Added Binding

--- WhatsNewWPF/Binding/BindingPage.xaml.cs
+++ WhatsNewWPF/Binding/BindingPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Controls;
 
 namespace WhatsNewWPF.Binding
 {
@@ -7,6 +9,66 @@ namespace WhatsNewWPF.Binding
         public BindingPage()
         {
             InitializeComponent();
+
+            BindingExpressionPanel.DataContext =
+            DelayBindingPanel.DataContext = new CustomerViewModel();
+        }
+
+        void BindingPage_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var binding = NameTextBox.GetBindingExpression(TextBox.TextProperty);
+            var textbox = binding.Target;
+            var vm = binding.ResolvedSource;
+        }
+    }
+
+    public class AppSettings
+    {
+        public static event EventHandler<PropertyChangedEventArgs> StaticPropertyChanged;
+
+        private static bool _enableNotifications;
+        public static bool EnableNotifications
+        {
+            get { return _enableNotifications; }
+            set
+            {
+                _enableNotifications = value;
+                OnStaticPropertyChanged("EnableNotifications");
+            }
+        }
+
+        public static void OnStaticPropertyChanged(string propertyName)
+        {
+            var handler = StaticPropertyChanged;
+            if (handler != null)
+            {
+                handler(null, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+    public class CustomerViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
