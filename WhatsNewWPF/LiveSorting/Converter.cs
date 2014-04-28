79b8d90947b0ea4c702bb18988e25925d785b133
Ben Dewey <bdewey01@hotmail.com>
2014-04-28 10:55:23 -0400

    Added LiveSorting

---
+++ WhatsNewWPF/LiveSorting/Converter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WhatsNewWPF.LiveSorting
+{
+    public class ChangeDirectionToGlyphConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var direction = value as ChangeDirection? ?? ChangeDirection.Neutral;
+            if (direction == ChangeDirection.Up)
+                return (char)0xE1FE;
+            if (direction == ChangeDirection.Down)
+                return (char)0xE1FC;
+            return (char)0x268A;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+    public class ChangeDirectionToForegroundConverter : IValueConverter
+    {
+        private readonly SolidColorBrush Green = new SolidColorBrush(Color.FromArgb(255, 65, 139, 0));
+        private readonly SolidColorBrush Red = new SolidColorBrush(Color.FromArgb(255, 200, 30, 0));
+        private readonly SolidColorBrush Blue = new SolidColorBrush(Color.FromArgb(255, 50, 100, 200));
+
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var direction = value as ChangeDirection? ?? ChangeDirection.Neutral;
+            if (direction == ChangeDirection.Up)
+                return Green;
+            if (direction == ChangeDirection.Down)
+                return Red;
+            return Blue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
--- WhatsNewWPF/LiveSorting/LiveSorting.xaml.cs
+++ WhatsNewWPF/LiveSorting/LiveSorting.xaml.cs
@@ -1,4 +1,12 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Threading;
 
 namespace WhatsNewWPF.LiveSorting
 {
@@ -7,6 +15,131 @@ namespace WhatsNewWPF.LiveSorting
         public LiveSortingPage()
         {
             InitializeComponent();
+            DataContext = new LiveSortingViewModel();
+            this.Loaded += OnLoaded;
         }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            var source = Resources["StockData"] as CollectionViewSource;
+            if (source == null) return;
+
+            var view = source.View as ICollectionViewLiveShaping;
+            if (view != null && view.CanChangeLiveSorting)
+            {
+                source.SortDescriptions.Add(new SortDescription("Price", ListSortDirection.Descending));                
+                view.IsLiveSorting = true;
+                view.LiveSortingProperties.Add("Price");
+            }
+        }
+
+        private void Activate_OnClick(object sender, RoutedEventArgs e)
+        {
+            var vm = Resources["ViewModel"] as LiveSortingViewModel;
+            if (vm == null) return;
+            if (Activate.Content.ToString() == "Stop")
+            {
+                vm.StopUpdates();
+                Activate.Content = "Activate";
+            }
+            else
+            {
+                vm.RandomlyUpdate();
+                Activate.Content = "Stop";
+            }
+        }
+    }
+
+    public class LiveSortingViewModel
+    {
+        private Random rand = new Random();
+        private DispatcherTimer _timer;
+        public ObservableCollection<StockTicker> Stocks { get; set; }
+
+        public LiveSortingViewModel()
+        {
+            Stocks = new ObservableCollection<StockTicker>()
+                {
+                    new StockTicker() { Name="Company AAA", Symbol = "AAA", Price = rand.NextDouble() * 100, ChangeDirection = GetDirection()},
+                    new StockTicker() { Name="Company BBB", Symbol = "BBB", Price = rand.NextDouble() * 100, ChangeDirection = GetDirection()},
+                    new StockTicker() { Name="Company CCC", Symbol = "CCC", Price = rand.NextDouble() * 100, ChangeDirection = GetDirection()},
+                    new StockTicker() { Name="Company DDD", Symbol = "DDD", Price = rand.NextDouble() * 100, ChangeDirection = GetDirection()},
+                    new StockTicker() { Name="Company EEE", Symbol = "EEE", Price = rand.NextDouble() * 100, ChangeDirection = GetDirection()}
+                };
+
+            _timer = new DispatcherTimer(DispatcherPriority.Normal);
+            _timer.Tick += UpdateStocks;
+            _timer.Interval = TimeSpan.FromMilliseconds(500);
+        }
+
+        private void UpdateStocks(object sender, EventArgs e)
+        {
+            var stocksToUpdate = Stocks.Where(s => rand.NextDouble() < 0.5);
+            foreach (var stock in stocksToUpdate)
+            {
+                var oldPrice = stock.Price;
+                var newPrice = oldPrice + rand.NextDouble() * 20 - 10;
+                stock.Price = Math.Max(newPrice, 0);
+                stock.ChangeDirection = GetDirection(oldPrice, stock.Price);
+            }
+        }
+
+        public ChangeDirection GetDirection(double oldPrice = 0d, double newPrice = 0d)
+        {
+            if (oldPrice == 0d && newPrice == 0d)
+            {
+                return rand.NextDouble() >= 0.5d ? ChangeDirection.Up : ChangeDirection.Down;
+            }
+            if (Math.Abs(oldPrice - newPrice) < .01)
+            {
+                return ChangeDirection.Neutral;
+            }
+            return oldPrice < newPrice ? ChangeDirection.Up : ChangeDirection.Down;
+        }
+
+        public void RandomlyUpdate()
+        {
+            _timer.Start();
+        }
+
+        public void StopUpdates()
+        {
+            _timer.Stop();
+        }
+    }
+
+    public class StockTicker : INotifyPropertyChanged
+    {
+        private double _price;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public ChangeDirection ChangeDirection { get; set; }
+        public double Price
+        {
+            get { return _price; }
+            set
+            {
+                _price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+    public enum ChangeDirection
+    {
+        Up,
+        Down,
+        Neutral
     }
 }
